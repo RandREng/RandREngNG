68359562827474e49522567828f416afa1e129e7
Robert Raboud <robert@randreng.com>
2024-12-06 11:39:18 -0500

    Made page component simplier

--- libs/randr-lib/src/lib/components/page/page.component.ts
+++ libs/randr-lib/src/lib/components/page/page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ContentChild, effect, inject, input, output, TemplateRef } from '@angular/core';
+import { Component, effect, inject, input, output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ErrorComponent } from '../error/error.component';
@@ -6,15 +6,12 @@ import { LoadingComponent } from '../loading/loading.component';
 import { LayoutService } from '../../services/layout.service';
 
 @Component({
-    selector: 'lib-randr-page',
-    imports: [CommonModule, ErrorComponent, LoadingComponent],
-    templateUrl: './page.component.html',
-    styleUrl: './page.component.scss'
+  selector: 'lib-randr-page',
+  imports: [CommonModule, ErrorComponent, LoadingComponent],
+  templateUrl: './page.component.html',
+  styleUrl: './page.component.scss'
 })
 export class PageComponent {
-  @ContentChild('loadingTemplate', { static: false }) loadingTemplate?: TemplateRef<unknown>;
-  @ContentChild('errorTemplate', { static: false }) errorTemplate?: TemplateRef<unknown>;
-
   error = input<Error | null>(null);
   isLoading = input<boolean>(false);
   title = input<string>('');
