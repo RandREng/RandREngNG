61d0463d2ee0dcc83da3398ebd3263c4aa0e8548
Robert Raboud <robert@randreng.com>
2025-05-11 12:12:09 -0400

    moving to HttpResource

--- libs/randr-lib/src/lib/services/http.service.ts
+++ libs/randr-lib/src/lib/services/http.service.ts
@@ -3,30 +3,27 @@ import {
   HttpErrorResponse,
   HttpEvent,
   HttpHeaders,
+  httpResource,
 } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { computed, effect, inject, Injectable, Signal, signal } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, finalize } from 'rxjs/operators';
 import { AlertService } from './alert.service';
 import { BusyService } from './busy.service';
+import { Page } from '../models/page.model';
+import { TableLazyLoadEvent } from 'primeng/table';
 
 @Injectable()
-export class HttpService {
+export abstract class HttpService {
   readonly httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
     }),
   };
 
-  httpClient: HttpClient;
-  alertService: AlertService;
-  busyService: BusyService;
-
-  constructor() {
-    this.httpClient = inject(HttpClient);
-    this.alertService = inject(AlertService);
-    this.busyService = inject(BusyService);
-  }
+  httpClient = inject(HttpClient);
+  alertService = inject(AlertService);
+  busyService = inject(BusyService);
 
   protected _get<T>(requestUrl: string): Observable<T> {
     this.busyService.AddBusy();
@@ -64,6 +61,25 @@ export class HttpService {
     );
   }
 
+
+  protected handleErrorSignal(error: HttpErrorResponse) {
+    if (error) {
+      if (error.error instanceof ErrorEvent) {
+        // A client-side or network error occurred. Handle it accordingly.
+        this.alertService.AddErrorMessage(
+          `An error occurred:, ${error.error.message}`,
+        );
+      } else {
+        // The backend returned an unsuccessful response code.
+        // The response body may contain clues as to what went wrong,
+        this.alertService.AddErrorMessage(
+          `Backend returned code ${error.status}, ` +
+          `body was: ${error.error}`,
+        );
+      }
+    }
+  }
+
   protected handleError(error: HttpErrorResponse): Observable<never> {
     //    this.busyService.RemoveBusy();
     if (error.error instanceof ErrorEvent) {
@@ -99,4 +115,70 @@ export class HttpService {
       observe: 'events',
     });
   }
+}
+
+export abstract class HttpPageService<T> extends HttpService {
+  abstract href: string;
+  public pageNumber = signal<number>(1);
+  public pageSize = signal<number>(10);
+  public sortColumn = signal<string | undefined>("");
+  public sortDirection = signal<string | undefined>("");
+  public id = signal<number | undefined>(undefined);
+
+  protected abstract filter: Signal<string>;
+
+  private dataResource = httpResource<Page<T>>(() => ({
+    url: this.href,
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+    },
+    params: {
+      pageSize: this.pageSize(),
+      page: this.pageNumber() + 1,
+      sortColumn: this.sortColumn() ?? '',
+      sortDirection: this.sortDirection() ?? '',
+      filter: this.filter(),
+    },
+  }));
+
+  data = computed(() => this.dataResource.value()?.results ?? [] as T[]);
+  error = computed(() => this.dataResource.error() as HttpErrorResponse);
+  totalRecords = computed(() => this.dataResource.value()?.rowCount ?? 0)
+  //   errorMessage = computed(() => setErrorMessage(this.error(), 'Vehicle'));
+  isLoading = this.dataResource.isLoading;
+
+  constructor() {
+    super();
+    effect(() => {
+      this.handleErrorSignal(this.error());
+    });
+    effect(() => {
+      if (this.isLoading()) {
+        this.busyService.AddBusy();
+      }
+      else if (!this.isLoading()) {
+        this.busyService.RemoveBusy()
+      }
+    });
+  }
+
+
+  public loadData(event: TableLazyLoadEvent) {
+    if (event.first != undefined && event.rows != undefined) {
+      this.pageNumber.set(event.first / event.rows);
+      this.pageSize.set(event.rows);
+    };
+
+    if (event.sortField && event.sortOrder) {
+      this.sortColumn.set(event.sortField as string);
+      this.sortDirection.set(event.sortOrder == 1 ? 'asc' : 'desc');
+    }
+  }
+
+  // get(id: string): Observable<T> {
+  //   const requestUrl = `${this.href}/${id}`;
+
+  //   return this._get<T>(requestUrl);
+  // }
 }
